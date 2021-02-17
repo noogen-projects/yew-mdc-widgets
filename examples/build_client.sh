2b4dc57946682d83aee4039d46f681ca9cfebec6
Alexander XX <freecoder.xx@gmail.com>
2021-02-17 23:58:13 +0300

    Change sh script to makefile

--- examples/build_client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mode=${1:+"--release"}
-flags=${1:+"-Copt-level=s"}
-out_dir=${1:-debug}
-
-RUSTFLAGS=$flags cargo build -p example_client --target wasm32-unknown-unknown $mode
-wasm-bindgen --target web --no-typescript --out-dir static/target --out-name example_client target/wasm32-unknown-unknown/${out_dir}/example_client.wasm
